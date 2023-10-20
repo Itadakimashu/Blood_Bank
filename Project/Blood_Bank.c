622f5569a7174cc8bd79c20bf10659ad9cefbea9
Itadakimashu <66174292+Itadakimashu@users.noreply.github.com>
2023-10-21 00:00:22 +0600

    ain

--- Project/Blood_Bank.c
+++ Project/Blood_Bank.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 #include<conio.h>
 #include<windows.h>
@@ -19,13 +20,14 @@ struct Donor{
 
 //global variables
 
-int donors_size = 2;
+int donors_size;
 
 char state[100] = "main_menu";
 char action;
 
 
 //functions
+void init(SD donors[]);
 void main_menu();
 void purchase_donor(SD donors[]);
 void add_donor(SD donors[]);
@@ -33,23 +35,13 @@ void edit_donor();
 void donated_list();
 void view_donor(SD donors[]);
 
+void save(SD donors[]);
+
 
 
 int main(){
-    struct Donor donors[MAX_DONORS];
-    SD d;
-    strcpy(d.name,"Nayer Ali");
-    strcpy(d.group,"b+");
-    strcpy(d.location,"NSU");
-    strcpy(d.contact, "18212312");
-    d.age = 15;
-    d.hemoglobin = 12.5;
-
-    donors[0] = d;
-    donors[1] = d;
-
-    printf("%s %d",d.name,d.contact);
-
+    SD donors[MAX_DONORS];
+    init(donors);
     while(state != "EXIT"){
         if(strcmp(state,"main_menu") == 0){
             main_menu();
@@ -74,6 +66,27 @@ int main(){
 }
 
 
+void init(SD donors[]){
+    donors_size = 0;
+    FILE *f = fopen("donors.txt","r");
+    while(1){
+        SD d;
+        if(!fgets(d.name,100,f)) break;
+
+        d.name[strcspn(d.name,"\n")] = '\0';
+        fscanf(f,"%s",d.group);
+        fscanf(f,"%s",d.location);
+        fscanf(f,"%s",d.contact);
+        fscanf(f,"%d",&d.age);
+        fscanf(f,"%f ",&d.hemoglobin);
+
+        //printf("%s %s %s %s %d %f\n",d.name,d.group,d.location,d.contact,d.age,d.hemoglobin);
+        donors[donors_size] = d;
+        donors_size++;
+
+    }
+}
+
 void main_menu(){
     system("cls");
     printf("<<<<<<<<<<<<<<<<<<<<<Welcome To blood bank>>>>>>>>>>>>>>>>>>>>\n\n");
@@ -83,7 +96,7 @@ void main_menu(){
     printf("4. view available Donors\n");
     printf("5. view donated list\n");
 
-    printf("press any option...\n");
+    printf("press any option...");
     action = getch();
 
     switch(action){
@@ -136,7 +149,6 @@ void purchase_donor(SD donors[]){
 
 void add_donor(SD donors[]){
     system("cls");
-    FILE *f = fopen("donors.txt","w");
     struct Donor d;
     printf("<<<<<<<<<<<<<<<<<< add donor >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n");
     printf("Donor Info          :-\n");
@@ -152,16 +164,12 @@ void add_donor(SD donors[]){
     scanf("%d",&d.age);
     printf("Donor hemoglobin    :- ");
     scanf("%f",&d.hemoglobin);
+    fflush(stdin);
 
     donors[donors_size] = d;
     donors_size++;
 
-    for(int i = 0; i < donors_size; i++){
-        struct Donor d = donors[i];
-        fprintf(f,"%s %s %s %s %d %f\n",d.name,d.group,d.location,d.contact,d.age,d.hemoglobin);
-    }
-    fclose(f);
-    printf("Donor added to database.\n");
+    save(donors);
     getch();
     strcpy(state, "main_menu");
 
@@ -180,11 +188,25 @@ void view_donor(SD donors[]){
     system("cls");
 
     printf("<<<<<<<<<<<<<<<<<<<<<view available donor>>>>>>>>>>>>>>>>>>>>>>>\n\n");
-    printf("          Name           group             location          contact\n");
+    printf("+----------------------------------------------------------------------------------------------------+\n");
+    printf("| SL |          Name         |    Age   |    Group   |  Hemoglobin |   Location   |      Contact     |\n");
+    printf("+----------------------------------------------------------------------------------------------------+\n");
+
+
     for(int i = 0; i < donors_size; i++){
         struct Donor d = donors[i];
         printf("%d. %s %s %s %s %d %f\n",i+1,d.name,d.group,d.location,d.contact,d.age,d.hemoglobin);
     }
     getch();
-    return;
+    strcpy(state,"main_menu");
+}
+
+void save(SD donors[]){
+    FILE *f = fopen("donors.txt","w");
+    for(int i = 0; i < donors_size; i++){
+        struct Donor d = donors[i];
+        fprintf(f,"%s\n%s\n%s\n%s\n%d\n%f\n",d.name,d.group,d.location,d.contact,d.age,d.hemoglobin);
+    }
+    fclose(f);
+    printf("Donor added to database.\n");
 }
